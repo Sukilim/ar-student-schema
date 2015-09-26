62ddfa1cbdc07b5c9987157d049b51468c404612
Sukilim <suki.fonxin@gmail.com>
2015-09-26 23:56:00 +0800

    Update 20121011144238_create_students.rb

--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -1,9 +1,24 @@
-require_relative '../config'
+require_relative '../../db/config'
 
-# this is where you should use an ActiveRecord migration to 
+class Student < ActiveRecord::Base
+# implement your Student model here
+validation_email :email, uniqueness: true { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/ }
+validation_age :age, numericality: {:greater_than_or_equal_to => 5 }
+validation_phone : phone_number?
 
-class CreateStudents < ActiveRecord::Migration
-  def change
-    # HINT: checkout ActiveRecord::Migration.create_table
-  end
+def phone_number?
+	if self.phone.scan(/\d/).count <10
+		error.add(:phone, "Please insert phone number again")
+
+	def name
+	"#{first_name} #{last_name}"
+	end
+
+	def age
+		now = Date.today
+		age = now.year - @student.birthday.year - ((now.month > @student.birthday.month || (now.month == @student.birthday.month && now.day >= @student.birthday.day)) ? 0 : 1)
+	end
 end
+
+# john = Student.new
+# john.age
